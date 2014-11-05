c9b0b1dadcf16ca13d1f747a7dbe201c1a453bde
Israel Calvete <icalvete@gmail.com>
2014-11-05 16:38:35 +0100

    Apache logs to local3

--- manifests/web/install.pp
+++ manifests/web/install.pp
@@ -14,28 +14,28 @@ class graphite::web::install {
 
   exec { 'graphite_source_tar':
     cwd     => $source_package_path,
-    creates => "$source_package_path/$graphite_source_file",
-    command => "/usr/bin/wget $url_source_tar/$graphite_source_file",
+    creates => "${source_package_path}/${graphite_source_file}",
+    command => "/usr/bin/wget ${url_source_tar}/${graphite_source_file}",
   }
 
   exec { 'graphite_source_untar':
     cwd     => $source_package_path,
-    command => "/bin/tar zxf $graphite_source_file",
+    command => "/bin/tar zxf ${graphite_source_file}",
     require => Exec['graphite_source_tar'],
-    onlyif  => "/usr/bin/test ! -d $source_package_path/$graphite_source_dir",
+    onlyif  => "/usr/bin/test ! -d ${source_package_path}/${graphite_source_dir}",
   }
 
   file { 'graphite_install_config':
     ensure  => 'present',
-    path    => "$source_package_path/$graphite_source_dir/setup.cfg",
+    path    => "${source_package_path}/${graphite_source_dir}/setup.cfg",
     content => template("${module_name}/web/setup.cfg.erb"),
     require => Exec['graphite_source_untar'],
   }
 
   exec { 'graphite_install_cmd':
-    cwd     => "$source_package_path/$graphite_source_dir",
+    cwd     => "${source_package_path}/${graphite_source_dir}",
     command => '/usr/bin/python setup.py install',
-    creates => "$install_path/$graphite_dirname/bin/run-graphite-devel-server.py",
+    creates => "${install_path}/${graphite_dirname}/bin/run-graphite-devel-server.py",
     require => File['graphite_install_config'],
   }
 }
